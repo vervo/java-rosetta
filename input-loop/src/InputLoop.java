0f190f1256c659b73e8475cba5a093c80f951905
Li-Wen Yin <super.invincible.sailor.vbn@gmail.com>
2014-08-24 17:33:27 +1200

    input-loop - complete.

--- input-loop/src/InputLoop.java
+++ input-loop/src/InputLoop.java
@@ -1,9 +1,26 @@
 
-public class InputLoop {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+
+
+public class InputLoop  {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		InputStreamReader sr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(sr);
+		
+		try {
+			String input = "";
+			
+			while((input = br.readLine()) != null){
+				System.out.println(input);
+			}
+			System.out.println(input);
+			
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
-
 }
